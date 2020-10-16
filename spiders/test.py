a9d411aa08236aa7aacb8db5d75a4a7f1ee4d576
itimor <itimor@126.com>
2020-10-16 22:35:31 +0800

    fix

---
+++ spiders/test.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# author: itimor
+
+for i in range(1, 7):
+    print(i)
--- spiders/zcaijing.py
+++ spiders/zcaijing.py
@@ -5,11 +5,17 @@
 import requests
 import re
 from lxml import etree
+import os
+import django
 
-# 忽略爬取
-ignore_tag1 = ['涨停板预测', '美股要闻', '市场动向', '主力研究', '热点专题', '股市名家', '炒股软件', '投稿专区', '股票公式', '股市战法', '大V论市', '基金', '黄金',
-               'MACD', '移动平均线', 'k线图', '股票技术指标']
-ignore_tag2 = ['港股投资入门', '缠论解盘', '缠论108课']
+os.environ['DJANGO_SETTINGS_MODULE'] = 'blog.settings'
+django.setup()
+
+from myblog.models import *
+
+ignore_category1 = ['涨停板预测', '美股要闻', '市场动向', '主力研究', '热点专题', '股市名家', '炒股软件', '投稿专区', '股票公式', '股市战法', '大V论市', '基金', '黄金',
+                    'MACD', '移动平均线', 'k线图', '股票技术指标']
+ignore_category2 = ['港股投资入门', '缠论解盘', '缠论108课']
 
 url = "https://www.zcaijing.com"
 r = requests.get(url).text
@@ -21,16 +27,20 @@ for ul in column_ul:
     tag1_li = column_li[0]
     code = tag1_li.xpath('./a/@href')[0].strip('/')
     name = tag1_li.xpath('./a/text()')[0]
-    if name in ignore_tag1:
+    if name in ignore_category1:
         continue
     tag2_li = column_li[1::]
     guo_li = []
     for li in tag2_li:
         code = li.xpath('./a/@href')[0].strip('/')
         name = li.xpath('./a/text()')[0]
-        if name in ignore_tag2:
+        if name in ignore_category2:
             continue
         url = f"https://www.zcaijing.com/{code}/"
         r = requests.get(url).text
         html = etree.HTML(r)
-        column_ul = html.xpath('//div[@class="content"]/ul[@class="column_ul"]')
+        page_li = html.xpath('//ul[@class="pagination"]/li[@class="page-item"]')
+        page_nums = len(page_li)
+        if page_nums > 1:
+            for page in range(1, page_nums):
+                url = f"https://www.zcaijing.com/{code}/p-{page}"
