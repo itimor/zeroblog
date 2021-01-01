4b1b62500878ccde271280e08982673676c77156
tomas <tomas@jjyy.com>
2021-01-01 17:47:47 +0800

    add

--- spider/spiders/zjlx_to_tg.py
+++ spider/spiders/zjlx_to_tg.py
@@ -8,27 +8,34 @@ from fake_useragent import UserAgent
 from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
+import tushare as ts
 import time
 import re
 import requests
 
 ua = UserAgent()
-
 headers = {'User-Agent': ua.random}
-engine = create_engine('sqlite:////Users/sha/data/zjlx/zjlx.db', echo=False, encoding='utf-8')
+
+# ts初始化
+ts_data = ts.pro_api('d256364e28603e69dc6362aefb8eab76613b704035ee97b555ac79ab')
+
+# 创建连接引擎
+engine = create_engine('sqlite:///zjlx.db', echo=False, encoding='utf-8')
 
 
 def get_stocks(num):
+    t1 = int(time.time() * 1000)
+    t2 = t1 - 31
     URL_stocks_infos = f'http://push2.eastmoney.com/api/qt/clist/get?pn=1&pz={num}&po=1&np=1&ut=b2884a393a59ad64002292a3e90d46a5&fltt=2&invt=2&fid0=f4001&fid=f69&fs=m:0+t:6+f:!2,m:0+t:13+f:!2,m:0+t:80+f:!2,m:1+t:2+f:!2,m:1+t:23+f:!2,m:0+t:7+f:!2,m:1+t:3+f:!2&stat=1&fields=f12,f14,f2,f3,f62,f184,f66,f69,f72,f75,f78,f81,f84,f87,f204,f205,f124&rt=53647283&cb=jQuery18307520567343556515_{t1}&_={t2}'
     r = requests.get(URL_stocks_infos, headers=headers).text
     X = re.split('}}', r)[0]
     X = re.split('"diff":', X)[1]
     df = pd.read_json(X, orient='records')
     if (len(df) != 0):
-        df2 = df[['f2', 'f3', 'f12', 'f14', 'f69', 'f75', 'f81', 'f87', 'f184']]
-        colunms_name = ['close', 'return_0', 'pre_code', 'name', 'super', 'big', 'mid', 'small', 'master']
+        df2 = df[['f124', 'f2', 'f3', 'f12', 'f14', 'f69', 'f75', 'f81', 'f87', 'f184']]
+        colunms_name = ['close_0', 'return_0', 'pre_code', 'name', 'super', 'big', 'mid', 'small', 'master']
         df2 = df2.rename(
-            columns={'Tdate': 'create_date', 'f12': colunms_name[2], 'f14': colunms_name[3], 'f2': colunms_name[0],
+            columns={'f124': 'create_date', 'f12': colunms_name[2], 'f14': colunms_name[3], 'f2': colunms_name[0],
                      'f3': colunms_name[1], 'f69': colunms_name[4], 'f75': colunms_name[5], 'f81': colunms_name[6],
                      'f87': colunms_name[7], 'f184': colunms_name[8]})
         df2['code'] = str(df2['pre_code'])
@@ -61,9 +68,9 @@ def send_tg(date, msg, chat_id):
     bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
 
 
-def main(date, num, tactics):
+def main(num, date, tactics):
     dfs = get_stocks(num)
-    display_name = ['code', 'name', 'super', 'big', 'mid', 'small', 'master', 'close', 'return_0']
+    display_name = ['create_date', 'code', 'name', 'super', 'big', 'mid', 'small', 'master', 'close_0', 'return_0']
     df = dfs.loc[
         (dfs["close"] < 50) &
         (dfs["super"] > 1) &
@@ -71,26 +78,27 @@ def main(date, num, tactics):
         (dfs["big"] > 1) &
         (dfs["mid"] < 0) &
         (dfs["small"] < 0), display_name]
-    df['return_1'] = np.nan
-    df['return_2'] = np.nan
-    df['return_3'] = np.nan
-    df[['create_date']] = date
+    df['create_date'] = pd.to_datetime(df['create_date'], unit='s').dt.strftime(date_format)
     print(df[:5])
     df.to_sql('zjlx_data', con=engine, index=False, if_exists='append')
 
 
 if __name__ == '__main__':
-    t1 = int(time.time() * 1000)
-    t2 = t1 - 31
-    t = 20
+    t = 18
+    tactics = ['master', 'super', 'big']
+    num = 2000
     date_format = '%Y-%m-%d'
+    d_format = '%Y%m%d'
     # 获得当天
     dd = datetime.now()
     if dd.hour > t:
-        cur_date = dd.strftime(date_format)
+        date = dd
     else:
-        cur_date = (dd - timedelta(1)).strftime(date_format)
-    tactics = ['master', 'super', 'big']
-    num = 2000
-    cur_date = '2020-12-27'
-    main(cur_date, num, tactics)
+        date = (dd - timedelta(1))
+    df = ts_data.trade_cal(exchange='', start_date=date.strftime(d_format), end_date=date.strftime(d_format),
+                           is_open='1')
+    print(df)
+    cur_date = date.strftime(date_format)
+    if len(df) > 0:
+        main(cur_date, num, tactics)
+
--- spider/spiders/zjlx_update_return.py
+++ spider/spiders/zjlx_update_return.py
@@ -8,54 +8,56 @@ from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
 import tushare as ts
-import easyquotation as es
 
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
 # ts初始化
 ts_data = ts.pro_api('d256364e28603e69dc6362aefb8eab76613b704035ee97b555ac79ab')
-df = ts_data.daily(ts_code='000001.SZ', start_date='20180718', end_date='20180718')
+# df = ts_data.daily(ts_code='000001.SZ', start_date='20180718', end_date='20180718')
 # print(df)
 # print(df.iat[0, 6])
 
-# es初始化
-eq = es.use('sina')  # 新浪 ['sina'] 腾讯 ['tencent', 'qq']
-
 # 创建连接引擎
-engine = create_engine('sqlite:////Users/sha/data/zjlx/zjlx.db', echo=False, encoding='utf-8')
+engine = create_engine('sqlite:///zjlx.db', echo=False, encoding='utf-8')
 
 
 def get_stocks(table, date, d1, d2):
     df = pd.read_sql_query(f'select * from {table} where create_date="{date}"', con=engine)
-    return_1 = []
-    return_2 = []
-    return_3 = []
+    close_1 = []
+    close_2 = []
+    close_3 = []
     for code in df['code']:
         code_info = ts_data.daily(ts_code=code, start_date=d1, end_date=d2)
         print(code)
         if len(code_info) > 0:
-            return_1.append(code_info.iat[2, 5])
-            return_2.append(code_info.iat[1, 5])
-            return_3.append(code_info.iat[0, 5])
+            close_1.append(code_info.iat[2, 5])
+            close_2.append(code_info.iat[1, 5])
+            close_3.append(code_info.iat[0, 5])
         else:
-            return_1.append(np.nan)
-            return_2.append(np.nan)
-            return_3.append(np.nan)
+            close_1.append(np.nan)
+            close_2.append(np.nan)
+            close_3.append(np.nan)
 
-    df['return_1'] = return_1
-    df['return_2'] = return_2
-    df['return_3'] = return_3
-    print(df.head())
-    df.to_sql(table, con=engine, index=False, if_exists='replace')
+    for i in range(1, 4):
+        df['close_' + str(i)] = eval('close_' + str(i))
+        df['return_' + str(i)] = (df['close_' + str(i)] - df['close_0']) / df['close_0']
+
+    df.round({'return_1': 2, 'return_2': 2, 'return_3': 2}).to_sql(table, con=engine, index=False, if_exists='replace')
 
 
 if __name__ == '__main__':
     table = 'zjlx_data'
     date_format = '%Y-%m-%d'
     d_format = '%Y%m%d'
-    date = '2020-12-27'
+    date = '2020-12-31'
     dd = datetime.strptime(date, date_format)
-    d1 = (dd + timedelta(1)).strftime(d_format)
-    d2 = (dd + timedelta(3)).strftime(d_format)
+    # 获取股票交易日期
+    start_date = (dd - timedelta(11)).strftime(d_format)
+    end_date = (dd - timedelta(1)).strftime(d_format)
+    df = ts_data.trade_cal(exchange='', start_date=start_date, end_date=end_date, is_open='1')
+    print(df)
+    d1 = df.iat[0, 1]
+    d2 = df.iat[2, 1]
     get_stocks(table, date, d1, d2)
+
