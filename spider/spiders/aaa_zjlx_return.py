ea3ef5e79ccb4e4cce9f56d512d46361faea7891
tomas <tomas@jjyy.com>
2021-01-09 17:21:33 +0800

    add

--- spider/spiders/aaa_zjlx_return.py
+++ spider/spiders/aaa_zjlx_return.py
@@ -8,6 +8,43 @@ import pandas as pd
 import numpy as np
 
 
+def gen_analysis(table):
+    df_rank = {'v1': 'return >= 9',
+               'v2': 'return >= 8 and return < 9',
+               'v3': 'return >= 7 and return < 8',
+               'v4': 'return >= 6 and return < 7',
+               'v5': 'return >= 7 and return < 6',
+               'v6': 'return >= 3 and return < 5',
+               'v7': 'return >= 0 and return < 3',
+               }
+
+    area_level = [i for i in range(31)]
+    label_level = [i for i in range(30)]
+    tactics = ['master', 'super', 'big', 'mid', 'small']
+
+    df = pd.DataFrame({
+        'l1': pd.Series(label_level),
+    })
+    for tactic in tactics:
+        tactic = f'{tactic}_x'
+        for rank_level, v in df_rank.items():
+            try:
+                df_a = pd.read_sql_query(f'select {tactic} from {table} where {v}', con=engine)
+            except:
+                continue
+            print(f'{table} {tactic} {rank_level}')
+            if len(df_a) > 0:
+                a = df_a[tactic].to_list()
+            else:
+                a = [0] * len(label_level)
+            cut = pd.cut(a, area_level, labels=label_level)
+            b = cut.value_counts().sort_index().to_list()
+            df[rank_level] = b
+            df[rank_level] = df[rank_level] / df[rank_level].sum()
+            df = df.replace(np.nan, 0)
+            df.to_sql(f'{table}_{tactic}', con=engine, index=False, if_exists='replace')
+
+
 def get_stocks():
     df_1600 = pd.read_sql_query(f'select code,name,close,return from {db}_1600', con=engine)
     last_df = pd.DataFrame()
@@ -34,8 +71,9 @@ def get_stocks():
             columns = ['code', 'name', 'close_x', 'return_x', 'master_x', 'super_x', 'big_x', 'mid_x', 'small_x']
             new_df = df_a.loc[(df_a["return_x"] > 0), columns]
             new_df.merge(df_1600, on=['code', 'name'])
-            new_table = f'{db}_{last_time}-{zjlx}'
+            new_table = f'{db}_{last_time}_{zjlx}'
             new_df.to_sql(new_table, con=engine, index=False, if_exists='replace')
+            gen_analysis(new_table)
             last_df = df
             last_time = zjlx
 
--- spider/spiders/new_zjlx_analysis.py
+++ spider/spiders/new_zjlx_analysis.py
@@ -6,48 +6,43 @@ from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
-import tushare as ts
 
-df_rank = {'v1': 'return_1 >= 9',
-           'v2': 'return_1 >= 8 and return_1 < 9',
-           'v3': 'return_1 >= 7 and return_1 < 8',
-           'v4': 'return_1 >= 6 and return_1 < 7',
-           'v5': 'return_1 >= 7 and return_1 < 6',
-           'v6': 'return_1 >= 3 and return_1 < 5',
-           'v7': 'return_1 >= 0 and return_1 < 3',
+df_rank = {'v1': 'return >= 9',
+           'v2': 'return >= 8 and return < 9',
+           'v3': 'return >= 7 and return < 8',
+           'v4': 'return >= 6 and return < 7',
+           'v5': 'return >= 7 and return < 6',
+           'v6': 'return >= 3 and return < 5',
+           'v7': 'return >= 0 and return < 3',
            }
 
 area_level = [i for i in range(31)]
 label_level = [i for i in range(30)]
-tactics = ['master', 'super', 'big']
+tactics = ['master', 'super', 'big', 'mid', 'small']
 
 
-def get_stocks(date):
-    d1 = pd.Series([date] * len(label_level))
-    l1 = pd.Series(label_level)
+def get_stocks(table):
     df = pd.DataFrame({
-        'date': d1,
-        'l1': l1,
+        'l1': pd.Series(label_level),
     })
-    for zjlx in tables:
-        table = f'{db}_{zjlx}'
-        for tactic in tactics:
-            for rank_level, v in df_rank.items():
-                try:
-                    df_a = pd.read_sql_query(f'select {tactic} from {table} where {v}', con=engine)
-                except:
-                    continue
-                print(f'{table} {tactic} {rank_level}')
-                if len(df_a) > 0:
-                    a = df_a[tactic].to_list()
-                else:
-                    a = [0] * len(label_level)
-                cut = pd.cut(a, area_level, labels=label_level)
-                b = cut.value_counts().sort_index().to_list()
-                df[rank_level] = b
-                df[rank_level] = df[rank_level] / df[rank_level].sum()
-                df = df.replace(np.nan, 0)
-                df.to_sql(f'{table}_{tactic}', con=engine, index=False, if_exists='replace')
+    for tactic in tactics:
+        tactic = f'{tactic}_x'
+        for rank_level, v in df_rank.items():
+            try:
+                df_a = pd.read_sql_query(f'select {tactic} from {table} where {v}', con=engine)
+            except:
+                continue
+            print(f'{table} {tactic} {rank_level}')
+            if len(df_a) > 0:
+                a = df_a[tactic].to_list()
+            else:
+                a = [0] * len(label_level)
+            cut = pd.cut(a, area_level, labels=label_level)
+            b = cut.value_counts().sort_index().to_list()
+            df[rank_level] = b
+            df[rank_level] = df[rank_level] / df[rank_level].sum()
+            df = df.replace(np.nan, 0)
+            df.to_sql(f'{table}_{tactic}', con=engine, index=False, if_exists='replace')
 
 
 if __name__ == '__main__':
@@ -58,14 +53,11 @@ if __name__ == '__main__':
     # 获得当天
     dd = datetime.now()
     cur_date = dd.strftime(date_format)
-    # ts初始化
-    ts_data = ts.pro_api('d256364e28603e69dc6362aefb8eab76613b704035ee97b555ac79ab')
-    d = dd.strftime(d_format)
-    df = ts_data.trade_cal(exchange='', start_date=d, end_date=d, is_open='1')
-    print(df)
-    if len(df) > 0 and dd.hour > 19:
+    if dd.hour > 19:
         tables = [datetime.strftime(x, t_format) for x in
                   pd.date_range(f'{cur_date} 09:50', f'{cur_date} 11:20:00', freq='10min')]
         # 创建连接引擎
         engine = create_engine(f'sqlite:///{cur_date}/{db}.db', echo=False, encoding='utf-8')
-        get_stocks(cur_date)
+        table = 'aaa_1000_1010'
+        get_stocks(table)
+
