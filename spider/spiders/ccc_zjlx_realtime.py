765f1ccb45dc8f75f9ba60ddbf7f635619f371e1
itimor <itimor@126.com>
2021-01-19 14:10:40 +0800

    add

--- spider/spiders/ccc_zjlx_realtime.py
+++ spider/spiders/ccc_zjlx_realtime.py
@@ -12,19 +12,16 @@ import tushare as ts
 import requests
 import re
 import json
+import time
 
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
-
-# 今日不涨  明日大涨 后天大大涨
-"""
-SELECT * from b_20210115 where ogc <-2 and super > 10 and change < 6 ORDER by super;
-"""
 
 
 def get_stocks(codes):
     dfs = []
     for pre_code in codes:
+        time.sleep(1)
         c = pre_code.split('.')
         code = f'{c[1].lower()}{c[0]}'
         url = f'http://hq.sinajs.cn/list={code}'
@@ -47,10 +44,11 @@ def get_stocks(codes):
     return df_a
 
 
-def send_tg(text, chat_id):
+def send_tg(date, msg, chat_id):
     token = '723532221:AAH8SSfM7SfTe4HmhV72QdLbOUW3akphUL8'
     bot = Bot(token=token)
     chat_id = chat_id
+    text = '%s 昨日涨幅>5今天低开前十\n' % date + msg
     bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
 
 
@@ -59,34 +57,34 @@ def main(date, table):
     dfs = get_stocks(df['code'].to_list())
     if len(dfs) > 0:
         new_df = pd.merge(df, dfs, how='inner', left_on=['code'], right_on=['code'])
-        columns = ['code', 'name', 'super', 'return', 'now', 'change', 'ogc']
-        df_a = pd.DataFrame()
-        if dd.hour > 8 and dd.hour < 10:
+        # 发送tg
+        if tg:
+            columns = ['code', 'name', 'return', 'now', 'change', 'ogc']
             df_a = new_df.loc[
                 # (new_df["ogc"] < -5) &
                 (new_df["change"] < 5)
-                , columns].sort_values(by=['ogc'], ascending=True)
+                , columns].sort_values(by=['change'], ascending=True)
+            print(df_a.head())
             if len(df_a) > 0:
                 last_df = df_a.head().round({'change': 2, 'ogc': 2}).to_string(header=None)
                 chat_id = "@hollystock"
-                text = '%s 昨日涨幅>5今天低开前十\n' % date + last_df
-                send_tg(text, chat_id)
-        if dd.hour > 13 and dd.hour < 15:
+                send_tg(date, last_df, chat_id)
             df_b = new_df.loc[
-                (new_df["super"] > 7) &
-                (new_df["ogc"] < -2) &
+                (new_df["return"] < 6) &
+                (new_df["return"] > 0) &
+                # (new_df["ogc"] < -5) &
                 (new_df["change"] < 5)
-                , columns].sort_values(by=['super'], ascending=True)
+                , columns].sort_values(by=['change'], ascending=False)
+            print(df_b.head())
             if len(df_b) > 0:
                 last_df = df_b.head().round({'change': 2, 'ogc': 2}).to_string(header=None)
                 chat_id = "@timorstock"
-                text = '%s 明日可能会涨\n' % date + last_df
-                send_tg(text, chat_id)
-        if dd.hour > 15:
+                send_tg(date, last_df, chat_id)
+        else:
             df_a = new_df.sort_values(by=['ogc'], ascending=True)
+            print(df_a.head())
             if dd.hour > 15:
                 df_a.to_sql(table, con=engine, index=False, if_exists='replace')
-        print(df_a.head())
 
 
 if __name__ == '__main__':
@@ -100,18 +98,25 @@ if __name__ == '__main__':
     end_date = dd - timedelta(days=1)
     cur_date = dd.strftime(date_format)
     cur_t = dd.strftime(t_format)
+    t_list = [datetime.strftime(x, t_format) for x in
+              pd.date_range(f'{cur_date} 09:16', f'{cur_date} 09:32:00', freq='2min')]
+    if cur_t in t_list:
+        tg = True
+    else:
+        tg = False
     if dd.hour > 8:
         # ts初始化
         ts_data = ts.pro_api('d256364e28603e69dc6362aefb8eab76613b704035ee97b555ac79ab')
         df = ts_data.trade_cal(exchange='', start_date=start_date.strftime(d_format),
                                end_date=end_date.strftime(d_format), is_open='1')
+        print(df)
         last_d = df.tail(1)['cal_date'].to_list()[0]
         # last_d = "20210116"
         last_day = datetime.strptime(last_d, d_format)
         last_date = last_day.strftime(date_format)
         # 创建连接引擎
         engine = create_engine(f'sqlite:///{last_date}/{db}.db', echo=False, encoding='utf-8')
-        table = f'b_{last_d}'
+        table = f'c_{last_d}'
         main(last_d, table)
         # table = f'c_{last_d}'
         # main(last_d, table)
